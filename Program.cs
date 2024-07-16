b9136142e64f38abf7ca01a10469e7fd4ff0dcbb
Bluscream <Bluscream@users.noreply.github.com>
2024-07-16 05:29:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace ConfigLocker {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Watchers/Ini.cs
@@ -0,0 +1,31 @@
+﻿using IniParser.Parser;
+using IniParser.Model;
+using System.IO;
+using IniParser;
+
+public class Ini {
+    private readonly IniData _data;
+
+    public void IniConfig(string filePath) {
+        _data = ReadIniFile(filePath);
+    }
+
+    private IniData ReadIniFile(string filePath) {
+        var parser = new IniParser();
+        return parser.ReadFile(filePath);
+    }
+
+    public string GetValue(string section, string key) {
+        return _data[section][key];
+    }
+
+    public void SetValue(string section, string key, string value) {
+        _data[section][key] = value;
+        WriteIniFile(section, key, value);
+    }
+
+    private void WriteIniFile(string section, string key, string value) {
+        var parser = new IniParser();
+        parser.WriteFile(_data, section, key, value);
+    }
+}
